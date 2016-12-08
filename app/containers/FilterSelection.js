c1515a0209361eed0148b7aedb967dd152d9378a
Yan <yanzhao3@illinois.edu>
2016-12-08 09:38:59 -0600

    remove time filter

--- app/containers/FilterSelection.js
+++ app/containers/FilterSelection.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux'
 import FilterSelectionComponent from '../components/FilterSelection'
-import { addSearchParameter, addSearchDataSource } from '../actions'
+import { addSearchParameter, addSearchDataSource, addStartDate, addEndDate } from '../actions'
 
 const mapStateToProps = (state, ownProps) => {
     return {
@@ -19,6 +19,14 @@ const mapDispatchToProps = (dispatch, ownProps) => {
                 const selectedDataSources = [];
                 dispatch(addSearchDataSource(selectedDataSources));
             }
+        },
+        onClearTime: () => {
+            //TODO: user the state of TimeFilter component
+            const minDate = new Date();
+            minDate.setFullYear(1983);
+            minDate.setHours(0, 0, 0, 0);
+            dispatch(addStartDate(minDate));
+            dispatch(addEndDate(new Date()));
         }
     }
 };
