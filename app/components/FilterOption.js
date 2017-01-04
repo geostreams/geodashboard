0e46d4524da9a9ff33482b4a02c942b7cee94dae
Michelle Pitcel <mpitcel2@illinois.edu>
2017-01-04 14:19:29 -0600

    Changed the checkbox options to use the Material UI styling

--- app/components/FilterOption.js
+++ app/components/FilterOption.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import styles from '../styles/filterOption.css'
+import Checkbox from 'material-ui/Checkbox';
 
 class FilterOption extends Component {
 	constructor(props) {
@@ -17,14 +18,12 @@ class FilterOption extends Component {
   	}
 
 	render() {
-		var checkedVar = (this.props.name == "data_source" && this.props.selectedDataSources.indexOf(this.props.id) > -1) || 
-						(this.props.name == "parameters" && this.props.selectedParameters.indexOf(this.props.id) > -1)
+		let checkedVar = (this.props.name == "data_source" && this.props.selectedDataSources.indexOf(this.props.id) > -1) ||
+						(this.props.name == "parameters" && this.props.selectedParameters.indexOf(this.props.id) > -1);
 		return (
-			<div className={styles.row}>
-				<div className={styles.col1}>
-					<input type="checkbox" name={this.props.name} value={this.props.id} onChange={this.handleChange.bind(this)} checked={checkedVar}></input>
-				</div>
-				<div className={styles.col2}>{this.props.label}</div>
+			<div className={styles.col_style}>
+				<Checkbox label={this.props.label} name={this.props.name} value={this.props.id}
+						  onCheck={this.handleChange.bind(this)} checked={checkedVar} />
 			</div>
 		);
 	}
