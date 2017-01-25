a1dbb81aee892ed0957077f0248bc414c234c152
Michelle Pitcel <mpitcel2@illinois.edu>
2017-01-25 12:08:17 -0600

    Changed the layout to have a side menu

--- app/components/Search.js
+++ app/components/Search.js
@@ -6,20 +6,29 @@ import Menu from './MenuPage'
 import { connect } from 'react-redux'
 import DownloadButtons from '../containers/DownloadButtons'
 import FilterSelection from '../containers/FilterSelection'
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
 
 class Search extends Component {
 
-	render() {
-		var title = 'Sensors';
-		return (
-			<div className={styles.root}>
+    render() {
+        var title = 'Sensors';
+        return (
+			<div>
 				<Menu selected="search"/>
-				<Map sensors={this.props.sensorsData}/>
-				<DownloadButtons/>
-				<FilterSelection/>
+				<div>
+					<Drawer width="380">
+						<AppBar title="Geodashboard 3.0" showMenuIconButton={false}/>
+						<FilterSelection/>
+						<DownloadButtons/>
+					</Drawer>
+				</div>
+				<div className={styles.root}>
+					<Map sensors={this.props.sensorsData}/>
+				</div>
 			</div>
-		);
-	}
+        );
+    }
 }
 
 export default Search
