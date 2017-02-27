5cafc76dcf14512e1ba1c1483a6faaf5674f58c4
Michelle Pitcel <mpitcel2@illinois.edu>
2017-02-27 13:35:23 -0600

    Added Exploratory Analysis Route

--- app/components/App.js
+++ app/components/App.js
@@ -3,6 +3,7 @@ import { Router, Route, browserHistory, hashHistory } from 'react-router'
 import Search from '../containers/Search'
 import Home from '../pages/Home'
 import Explore from '../pages/Explore'
+import Analysis from './Analysis';
 import About from '../pages/About';
 import RouteMismatch from '../pages/RouteMismatch'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -22,6 +23,7 @@ class App extends Component {
                     <Route path="/" component={Home}/>
                     <Route path="/explore" component={Explore}/>
                     <Route path="/search" component={Search}/>
+                    <Route path="/analysis" component={Analysis}/>
                     <Route path="/about" component={About}/>
                     <Route path="*" component={RouteMismatch}/>
                 </Router>
