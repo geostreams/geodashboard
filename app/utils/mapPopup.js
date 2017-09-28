57cf5815f6f39609876fb891c1df42d995b653c7
Michelle Pitcel <mpitcel2@illinois.edu>
2017-09-28 14:08:04 -0500

    Create Region Popup

--- app/utils/mapPopup.js
+++ app/utils/mapPopup.js
@@ -140,3 +140,79 @@ export function removePopup(theMap){
         closer.blur();
     }
 }
+
+export function popupRegion(feature: ol.Feature, styles){
+
+    let id = feature.getId().toUpperCase();
+    let sensorInfo = feature.attributes;
+
+    let sensorTrends = sensorInfo.trend_type;
+    let trendColor = sensorInfo.trend_color;
+    let trendValues = sensorInfo.trend_values;
+
+    let trendsLeft = '';
+    let trendsRight = '';
+
+    let startTime = new Date(sensorInfo.minStartTime).toLocaleDateString();
+    let endTime = new Date(sensorInfo.maxEndTime).toLocaleDateString();
+    let timePeriod = '<tr><td><strong>Time Period: </strong></td>'.concat('<td>', startTime, ' - ',
+        endTime, '</td></tr>');
+
+    let sourceColor = sensorInfo.color;
+
+    let headerText = '<h2 class=' + styles.header2style + ' style="background-color: ' +
+        sourceColor + ';">' + id + '</h2>';
+
+    let bodyText =
+        '<table class=' + styles.popup_table + '>' +
+            timePeriod +
+        '</table>' +
+        '<div class=' + styles.greyborder + '></div>';
+
+    if (sensorTrends == "noTrend" || sensorTrends == "") {
+
+        let leftText = " ";
+        trendsLeft = '<tr><td rowspan="5"><p class=' + styles.noValue + ' style="background: ' +
+            trendColor + '; border-color: ' + trendColor + ';">' + leftText + '</p></td></tr>';
+
+        let rightText = "Not enough data to display";
+        trendsRight = '' +
+            '<tr><td><strong>' + rightText + '</strong></td></tr>';
+
+    } else {
+
+        if (sensorTrends == 'trendUp' || sensorTrends == 'overThresholdUp') {
+            trendsLeft = '<tr><td rowspan="5"><p class=' + styles.upArrow + ' style="background: ' +
+                trendColor + '; border-color: ' + trendColor + '; ">' + trendValues[4] + '</p></td></tr>';
+        } else if (sensorTrends == 'trendDown' || sensorTrends == 'overThresholdDown') {
+            trendsLeft = '<tr><td rowspan="5"><p class=' + styles.downArrow + ' style="background: ' +
+                trendColor + '; border-color: ' + trendColor + ';">' + trendValues[4] + '</p></td></tr>';
+        } else if (sensorTrends == 'noTrend') {
+            trendsLeft = '<tr><td rowspan="5"><p class=' + styles.noValue + ' style="background: ' +
+                trendColor + '; border-color: ' + trendColor + ';">' + trendValues[4] + '</p></td></tr>';
+        } else {
+            trendsLeft = '<tr><td rowspan="5"><p class=' + styles.noValue + ' style="background: ' +
+                trendColor + '; border-color: ' + trendColor + ';">' + trendValues[4] + '</p></td></tr>';
+        }
+
+        trendsRight = '' +
+            '<tr><td><strong>Baseline Avg: </strong>' + trendValues[0] + '</td></tr>' +
+            '<tr><td><strong>Rolling Avg: </strong>' + trendValues[1] + '</td></tr>' +
+            '<tr><td><strong>Latest Value: </strong>' + trendValues[2] + '</td></tr>' +
+            '<tr><td><strong>Latest Time: </strong>' + trendValues[3] + '</td></tr>';
+    }
+
+    let trends = trendsLeft + trendsRight;
+
+    let regionText =
+        '<table class=' + styles.tablestyle + '>' +
+            trends +
+        '</table>';
+
+    if(sensorInfo.trends_detail) {
+        regionText += '<a href="#/trendsdetail/region/'+ sensorInfo.location + '" class=' + styles.viewdetail +
+            ' style="background-color: ' + sourceColor + ';">View Details for the ' +
+            sensorInfo.region + ' Region </a>';
+    }
+    return headerText + bodyText + regionText;
+}
