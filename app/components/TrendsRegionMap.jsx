9c7e07f4326eecc6aad5c1c8dc56160002106dbb
Michelle Pitcel <mpitcel2@illinois.edu>
2017-09-28 14:07:34 -0500

    Update popup call

--- app/components/TrendsRegionMap.jsx
+++ app/components/TrendsRegionMap.jsx
@@ -8,7 +8,7 @@ require("openlayers/css/ol.css");
 import styles from '../styles/map.css';
 import {Icon} from 'react-mdc-web';
 import {getTrendColor, getCustomLocation} from '../utils/getConfig';
-import {popupHeader, popupTrends} from '../utils/mapPopup';
+import {popupRegion} from '../utils/mapPopup';
 import {sensorsToFeaturesTrendPage, getAttribution} from '../utils/mapUtils';
 import {drawHelper, centerHelper} from '../utils/mapDraw';
 import type {MapProps, TrendsMapState} from '../utils/flowtype';
@@ -67,7 +67,7 @@ class TrendsRegionMap extends Component {
         const content = document.getElementById('popup-content');
         if (feature && feature.getId()) {
 
-            let popupText = popupHeader(feature, styles) + popupTrends(feature, styles);
+            let popupText = popupRegion(feature, styles);
 
             if (content) {
                 content.innerHTML = popupText;
