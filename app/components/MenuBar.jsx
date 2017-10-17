b1acf4876ab2c9c19ab7945656a2ac27987d368b
Michelle Pitcel <mpitcel2@illinois.edu>
2017-10-17 10:54:16 -0500

    Add Trends Button

--- app/components/MenuBar.jsx
+++ app/components/MenuBar.jsx
@@ -1,11 +1,26 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router'
 import styles from '../styles/menuBar.css';
+import {
+    Toolbar, ToolbarRow, ToolbarSection, ToolbarTitle,
+    Button, MenuItem, Menu, MenuAnchor
+} from 'react-mdc-web';
 
 class MenuBar extends Component {
-    constructor(props:Object) {
+    state: {
+        openMenu: boolean
+    };
+
+    constructor(props: Object) {
         super(props);
+        this.state = {
+            openMenu: false
+        };
     }
+
+    onClickTrendButton(openMenuValue: boolean) {
+        this.setState({openMenu: openMenuValue});
+    };
 
     render() {
         let logo;
@@ -33,11 +48,47 @@ class MenuBar extends Component {
                             <hr className={styles.header_hr} />
                         </div>
                             <ul className={styles.navbar} id="navigation">
-                                <li className={this.props.selected === "home"? styles.active: '' }><a href="/" >HOME</a></li>
-                                <li className={this.props.selected === "explore"? styles.active: '' }><a href="/geodashboard/#explore">EXPLORE</a></li>
-                                <li className={this.props.selected === "compare"? styles.active: '' }><a href="/geodashboard/#compare">COMPARE</a></li>
-                                <li className={this.props.selected === "search"? styles.active: '' }><Link to="/search" >SEARCH</Link></li>
-                                <li className={this.props.selected === "about"? styles.active: '' }><a href="/geodashboard/#about">ABOUT</a></li>
+                                <li className={this.props.selected === "home"? styles.active: '' }>
+                                    <a href="/" >HOME</a>
+                                </li>
+                                <li className={this.props.selected === "explore"? styles.active: '' }>
+                                    <a href="/geodashboard/#explore">EXPLORE</a>
+                                </li>
+                                <li className={this.props.selected === "compare"? styles.active: '' }>
+                                    <a href="/geodashboard/#compare">COMPARE</a>
+                                </li>
+                                <li className={this.props.selected === "search"? styles.active: '' }>
+                                    <Link to="/search" >SEARCH</Link>
+                                </li>
+                                <li className={this.props.selected === "trends"? styles.active: '' }>
+                                    <Button className={styles.trendsButton}
+                                            onClick={this.onClickTrendButton.bind(this, true)} >
+                                        TRENDS
+                                    </Button>
+                                </li>
+                                <li className={this.props.selected === "about"? styles.active: '' }>
+                                    <a href="/geodashboard/#about">ABOUT</a>
+                                </li>
+                                <MenuAnchor className={styles.menu_anchor_style}>
+                                    <Menu className={styles.menu_style}
+                                          open={this.state.openMenu}
+                                          onClose={this.onClickTrendButton.bind(this, false)}>
+                                        <MenuItem>
+                                            <Button>
+                                                <Link className={styles.button_style} href={"#trendsstations"}>
+                                                    TRENDS STATIONS
+                                                </Link>
+                                            </Button>
+                                        </MenuItem>
+                                        <MenuItem>
+                                            <Button>
+                                                <Link className={styles.button_style} href={"#trendsregions"}>
+                                                    TRENDS REGIONS
+                                                </Link>
+                                            </Button>
+                                        </MenuItem>
+                                    </Menu>
+                                </MenuAnchor>
                             </ul>
                         </div>
                 </div>
