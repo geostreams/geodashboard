a4e259ffbef2029965e0471d186c8a4d878cb6a2
Michelle Pitcel <mpitcel2@illinois.edu>
2017-10-24 16:53:57 -0500

    Graph values and deviations

--- app/components/RegionChart.jsx
+++ app/components/RegionChart.jsx
@@ -4,6 +4,7 @@
 
 import React, { Component } from 'react';
 import rd3 from 'react-d3';
+import d3 from 'd3';
 
 class RegionChart extends Component {
 
@@ -13,44 +14,105 @@ class RegionChart extends Component {
 
     render() {
 
-        let chartTitle = this.props.selectedParameter;
+        let chartTitle = 'No Chosen Parameter';
         let trendsPageSettings = this.props.trends_settings;
         if (trendsPageSettings) {
             trendsPageSettings.map(p => {
-                    if (p.parameter.id == this.props.selectedParameter) {
-                        chartTitle = p.parameter.title;
-                    }
+                if (p.parameter.id == this.props.selectedParameter) {
+                    chartTitle = p.parameter.title;
                 }
-            )
+            })
+        }
+
+        let trendsRegionName = this.props.trends_region_name;
+        let trendRegions = this.props.trendRegions;
+
+        let trendDataRaw = [];
+        let trendData = [];
+        let trendDeviationDataRaw = [];
+        let trendPositiveDeviation = [];
+        let trendNegativeDeviation = [];
+        for (let i = 1 ; i < trendRegions.length; i++) {
+
+            if (trendRegions[i].name == trendsRegionName) {
+
+                // The Values
+                trendRegions[i].trends_detail.map( d => {
+                    for (let x in d )
+                    {
+                        trendDataRaw.push({"x": x, "y": d[x]})
+                    }
+                });
+                trendDataRaw.sort(function(a, b) {
+                    return Number(a.x) - Number(b.x);
+                });
+
+                // The Deviations
+                trendRegions[i].trends_deviation.map( d => {
+                    for (let y in d )
+                    {
+                        trendDeviationDataRaw.push({"x": y, "y": d[y]})
+                    }
+                });
+                trendDeviationDataRaw.sort(function(a, b) {
+                    return Number(a.x) - Number(b.x);
+                });
+
+                // The Positive Deviations
+                for (let g = 0; g < trendDeviationDataRaw.length; g++) {
+                    trendPositiveDeviation.push({
+                        "x":trendDeviationDataRaw[g].x,
+                        "y":(Number(trendDataRaw[g].y) + Number(trendDeviationDataRaw[g].y))
+                    });
+                }
+
+                // The Negative Deviations
+                for (let g = 0; g < trendDeviationDataRaw.length; g++) {
+                    trendNegativeDeviation.push({
+                        "x":trendDeviationDataRaw[g].x,
+                        "y":(Number(trendDataRaw[g].y) - Number(trendDeviationDataRaw[g].y))
+                    });
+                }
+
+                trendData.push(
+                    {
+                        name: "Base Value",
+                        values: trendDataRaw,
+                        circleRadius: Number(5)
+                    },
+                    {
+                        name: "Positive",
+                        values: trendPositiveDeviation,
+                        circleRadius: Number(1)
+                    },
+                    {
+                        name: "Negative",
+                        values: trendNegativeDeviation,
+                        circleRadius: Number(1)
+                    }
+                )
+            }
+
         }
 
         let LineChart = rd3.LineChart;
-        let trendData = [
-            {
-                name: "Line 1",
-                values: [{ x: 0, y: 0 },{ x: 50, y: 20 },{ x: 100, y: 40 }]
-            },
-            {
-                name: "Line 2",
-                values: [{ x: 0, y: 40 },{ x: 50, y: 40 },{ x: 100, y: 60 }]
-            },
-            {
-                name: "Line 3",
-                values: [{ x: 0, y: 80 },{ x: 50, y: 60 },{ x: 100, y: 80}]
-            }
-        ];
 
-        return (
-
+        let lineChart = (
             <LineChart
                 data={trendData}
-                width={500}
-                height={500}
-                title={chartTitle}
-                yAxisLabel="Amount (unit)"
-                xAxisLabel="Time (unit)"
+                title={chartTitle} legend={true}
+                width={600} height={400}
+                yAxisLabel="Amount" yAxisLabelOffset={Number(50)}
+                xAxisLabel="Time" xAxisLabelOffset={Number(50)}
+                gridHorizontal={true} gridVertical={true}
+                hoverAnimation={true}
+                colors={d3.scale.category10()}
+                margins={{top: 10, right: 120, bottom: 50, left: 100}}
             />
+        );
 
+        return (
+            <div>{lineChart}</div>
         )
     }
 }
