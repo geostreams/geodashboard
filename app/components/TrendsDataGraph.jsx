07b9b19342f52c1f422d900ac1fb7bcb3aa67fd3
Michelle Pitcel <mpitcel2@illinois.edu>
2017-11-07 14:09:58 -0600

    Add new component

---
+++ app/components/TrendsDataGraph.jsx
@@ -0,0 +1,34 @@
+/*
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import LineChartWithDeviations from '../containers/LineChartWithDeviations';
+
+class TrendsDataGraph extends Component {
+
+    constructor(props: Object) {
+        super(props);
+    }
+
+    componentWillMount() {
+        const {loadDetailSensor} = this.props;
+        loadDetailSensor(this.props.selectedParameter, this.props.selectedSeason, this.props.trends_region);
+
+    }
+
+    render() {
+        return (
+
+            <LineChartWithDeviations
+                trends_settings={this.props.trends_settings}
+                trends_region_name={this.props.trends_region_name}
+                trends_region={this.props.trends_region}
+            />
+
+        );
+    }
+
+}
+
+export default TrendsDataGraph
