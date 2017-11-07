264cddbfe9cb9f775dea32dd113779adcfd8bbe4
Michelle Pitcel <mpitcel2@illinois.edu>
2017-11-07 17:05:40 -0600

    Update this file

--- app/containers/LineChartWithDeviations.js
+++ app/containers/LineChartWithDeviations.js
@@ -4,14 +4,28 @@
 
 import { connect } from 'react-redux';
 import LineChartWithDeviationsComponent from '../components/LineChartWithDeviations';
+import type { Dispatch } from '../utils/flowtype';
+import { fetchRegionDetailTrends } from '../actions/index';
+
 
 const mapStateToProps = (state) => {
     return {
         trendRegions: state.chosenTrends.trends_regions,
-        selectedParameter: state.chosenTrends.parameter
+        selectedParameter: state.chosenTrends.parameter,
+        selectedSeason: state.chosenTrends.season,
+        detailRegion: state.chosenTrends.detail_region
     }
 };
 
-const LineChartWithDeviations = connect(mapStateToProps)(LineChartWithDeviationsComponent);
+
+const mapDispatchToProps = (dispatch:Dispatch) => {
+    return {
+        loadDetailSensor: (parameter, season, region) => {
+            dispatch(fetchRegionDetailTrends(parameter, season, region))
+        }
+    }
+};
+
+const LineChartWithDeviations = connect(mapStateToProps, mapDispatchToProps)(LineChartWithDeviationsComponent);
 
 export default LineChartWithDeviations
