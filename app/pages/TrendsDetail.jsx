956e6cf354943c618bc3713b97c597dfd8b77957
Michelle Pitcel <mpitcel2@illinois.edu>
2017-11-08 14:12:12 -0600

    refactoring

--- app/pages/TrendsDetail.jsx
+++ app/pages/TrendsDetail.jsx
@@ -5,14 +5,13 @@
 
 import React, {Component, PropTypes} from 'react';
 import Menu from '../containers/MenuBar';
-import TrendsParameters from '../containers/TrendsParameters';
 import RegionMiniMap from '../containers/RegionMiniMap';
 import TrendsRegionDetails from '../containers/TrendsRegionDetails';
 import LineChartWithDeviations from '../containers/LineChartWithDeviations';
 import {Grid, Cell, Content, List, Card, CardTitle} from 'react-mdc-web';
 import styles from '../styles/main.css';
 import trendsStyles from '../styles/trends.css';
-import { getCustomTrendsRegion, getTrendsPageSettings, getTrendsDefaultValues } from '../utils/getConfig';
+import { getCustomTrendsRegion, getTrendsPageSettings } from '../utils/getConfig';
 import {Link} from 'react-router';
 
 
@@ -21,8 +20,6 @@ class TrendsDetail extends Component {
     render() {
 
         let trendsPageSettings = getTrendsPageSettings();
-        let trendsPageDefaults = getTrendsDefaultValues();
-        let trendsThresholdChoice = false;
         let trendsRegionsSensors = [];
         let trendsRegionTitle =  getCustomTrendsRegion(this.props.params.region);
         let trendsRegionTitleLink =  " / " + getCustomTrendsRegion(this.props.params.region);
@@ -42,13 +39,6 @@ class TrendsDetail extends Component {
                                     </CardTitle>
                                 </Card>
                                 <List className={trendsStyles.detailListStyle}>
-                                    <TrendsParameters
-                                        className={trendsStyles.parametersCardStyleDetail}
-                                        trends_settings={trendsPageSettings}
-                                        trends_threshold_choice={trendsThresholdChoice}
-                                        trends_defaults={trendsPageDefaults}
-                                        trends_detail_page={true}
-                                    />
                                     <TrendsRegionDetails
                                         trends_region_name={trendsRegionName}
                                     />
