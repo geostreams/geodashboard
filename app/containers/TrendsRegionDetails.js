4a403c6cd6b40d61feaef8bd90a4bf81f991b14d
Michelle Pitcel <mpitcel2@illinois.edu>
2017-11-14 09:30:22 -0600

    Add variable

--- app/containers/TrendsRegionDetails.js
+++ app/containers/TrendsRegionDetails.js
@@ -9,6 +9,7 @@ const mapStateToProps = (state) => {
     return {
         regionsStations: state.chosenTrends.trends_regions,
         selectedParameter: state.chosenTrends.parameter,
+        selectedSeason: state.chosenTrends.season,
     }
 };
 
