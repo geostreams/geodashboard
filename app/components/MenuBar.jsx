2d3a4dfa04dd974ee741064dadc5d5a85ca4ad99
Michelle Pitcel <mpitcel2@illinois.edu>
2017-11-28 10:41:28 -0600

    changes: - refactoring - add icon

--- app/components/MenuBar.jsx
+++ app/components/MenuBar.jsx
@@ -3,7 +3,7 @@ import {Link, hashHistory} from 'react-router';
 import styles from '../styles/menuBar.css';
 import {
     Toolbar, ToolbarRow, ToolbarSection, ToolbarTitle,
-    Button, MenuItem, Menu, MenuAnchor
+    Button, MenuItem, Menu, MenuAnchor, Icon
 } from 'react-mdc-web';
 import {getApplicationOptions} from "../utils/getConfig";
 
@@ -49,28 +49,32 @@ class MenuBar extends Component {
         applicationOptions.pages.map( page =>
         {
 	        if(page.url) {
-		        pageLinks.push(<li key={page.name} className={this.props.selected === page.name.toLowerCase() ? styles.active: ''}> <Link href={page.url}>{page.name}</Link> </li>);
+		        pageLinks.push(
+		            <li key={page.name} className={this.props.selected === page.name.toLowerCase() ? styles.active: ''}>
+                        <Link href={page.url}>{page.name}</Link>
+                    </li>
+                );
 	        } else if(page.children){
 
                 const menuItems = page.children.map(item =>
-                    <MenuItem  role="menuitem" key={item.name} onClick={() => this.onClickMenuItem(item.url)}> {item.name} </MenuItem>
+                    <MenuItem role="menuitem" key={item.name} onClick={() => this.onClickMenuItem(item.url)}>
+                        {item.name}
+                    </MenuItem>
                 );
                 pageLinks.push(
                     <li key={page.name}>
                         <span className={styles.navigation_item}
                               onClick={() => this.toggleTrendMenu(true)}
-
                         >
-                            {page.name}
+                            {page.name}<Icon className={styles.buttonTrends} name="arrow_drop_down"/>
                         </span>
                         <MenuAnchor className={styles.inline}>
-
-                        <Menu className={styles.menu_style}
-                              open={this.state.openMenu}
-                              onClose={() => this.toggleTrendMenu(false)}
-                        >
-                            {menuItems}
-                        </Menu>
+                            <Menu className={styles.menu_style}
+                                  open={this.state.openMenu}
+                                  onClose={() => this.toggleTrendMenu(false)}
+                            >
+                                {menuItems}
+                            </Menu>
                         </MenuAnchor>
                     </li>
                 )
@@ -81,12 +85,16 @@ class MenuBar extends Component {
             <header>
                 <div className={styles.header_background}>
                     <div className={styles.header_banner} >
-                    {logo}
+                        {logo}
                         <div className={styles.header_title}>
-                            <p id="header-title-text" className={styles.header_title_text}>{this.props.header_title}</p>
+                            <p id="header-title-text" className={styles.header_title_text}>
+                                {this.props.header_title}
+                            </p>
                         </div>
                         <div className={styles.header_subtitle}>
-                            <p id="header-subtitle-text" className={styles.header_subtitle_text}>{this.props.subtitle}</p>
+                            <p id="header-subtitle-text" className={styles.header_subtitle_text}>
+                                {this.props.subtitle}
+                            </p>
                         </div>
                         <div className={styles.header_hr_div}>
                             <hr className={styles.header_hr} />
@@ -94,7 +102,7 @@ class MenuBar extends Component {
                             <ul className={styles.navbar} id="navigation">
                                 {pageLinks}
                             </ul>
-                        </div>
+                    </div>
                 </div>
             </header>
         );
