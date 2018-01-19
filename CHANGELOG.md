144d5c7f706b7b1a42d974d8a8a8eda5fd4886b3
indiragp <indiragp@illinois.edu>
2018-01-19 14:21:48 -0600

    Missing import statement, formatting selectedSearch.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/) 
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## UNRELEASED
+## [Unreleased]
 
 ### Added
 - Add Explore Layers option on the Explore Page as a Drawer
--- app/reducers/selectedSearch.js
+++ app/reducers/selectedSearch.js
@@ -2,35 +2,39 @@
  * @flow
  */
 import {ADD_SEARCH_DATASOURCE, ADD_SEARCH_PARAMETER, ADD_SEARCH_LOCATION, UPDATE_AVAILABLE_FILTERS,
-    ADD_START_DATE, ADD_END_DATE } from '../actions'
+    ADD_START_DATE, ADD_END_DATE, RECEIVE_SENSORS } from '../actions'
 import {collectSources, collectLocations, collectParameters, collectDates} from './sensors'
 import type { selectedSearchState, Sensors, DatasourceParameter, Location } from '../utils/flowtype'
 import {intersectArrays} from '../utils/arrayUtils'
 
-type SelectedSearchAction = {| type:string, sensors:Sensors, data_sources: DatasourceParameter, parameters:DatasourceParameter,
-    locations:Location, data_sources:Array<string>,  parameter:Array<string>,  location:?string, date:?Date,
-    availableSensors:Sensors, selected_filters:Array<string>, allFilters:Array<Object> |};
+type SelectedSearchAction = {| type:string, sensors:Sensors, data_sources: DatasourceParameter,
+    parameters:DatasourceParameter, locations:Location, data_sources:Array<string>,  parameter:Array<string>,
+    location:?string, date:?Date, availableSensors:Sensors, selected_filters:Array<string>, allFilters:Array<Object> |};
 
 const defaultState = {  data_sources: {selected: [], available: []},
                         parameters: {selected: [], available: []},
                         locations: {selected: null, available: []},
-                        dates: {selected: {start: null, end: null}, available: {start: new Date("1951-04-10"), end: new Date()}}
+                        dates: {selected: {start: null, end: null},
+                            available: {start: new Date("1951-04-10"), end: new Date()}}
                     };
 
 const selectedSearch = (state:selectedSearchState = defaultState, action:SelectedSearchAction) => {
     switch(action.type) {
         case ADD_SEARCH_DATASOURCE:
-            const tempState = Object.assign({}, state, {data_sources: {selected: action.data_sources, available: state.data_sources.available}});
+            const tempState = Object.assign({}, state,
+                {data_sources: {selected: action.data_sources, available: state.data_sources.available}});
             const newState = updateSelected(action.selected_filters, tempState, 'data_sources');
             return Object.assign({}, state, newState);
 
         case ADD_SEARCH_PARAMETER:
-            const tempStateP = Object.assign({}, state, {parameters: {selected: action.parameter, available: state.parameters.available}});
+            const tempStateP = Object.assign({}, state,
+                {parameters: {selected: action.parameter, available: state.parameters.available}});
             const newStateP = updateSelected(action.selected_filters, tempStateP, 'parameters');
             return Object.assign({}, state, newStateP);
 
         case ADD_SEARCH_LOCATION:
-            const tempStateL = Object.assign({}, state, {locations: {selected: action.location, available: state.locations.available}});
+            const tempStateL = Object.assign({}, state,
+                {locations: {selected: action.location, available: state.locations.available}});
             const newStateL = updateSelected(action.selected_filters, tempStateL, 'locations');
             return Object.assign({}, state, newStateL);
 
@@ -106,27 +110,33 @@ function updateAvailable(sensors, selected_filters, allFilters, state){
                 case 'data_sources':
                     const newAvailableDataSources = collectSources(sensors);
                     const newSelectedDataSources = intersectArrays(newAvailableDataSources, state.data_sources.selected);
-                    newState = Object.assign({}, newState, {data_sources: {selected: newSelectedDataSources, available: newAvailableDataSources}} );
+                    newState = Object.assign({}, newState,
+                        {data_sources: {selected: newSelectedDataSources, available: newAvailableDataSources}} );
                     return;
                 case 'parameters':
                     const newAvailableParameters = collectParameters(sensors);
                     const newSelectedParameters = intersectArrays(newAvailableParameters, state.parameters.selected);
-                    newState = Object.assign({}, newState, {parameters: {selected: newSelectedParameters, available: newAvailableParameters}});
+                    newState = Object.assign({}, newState,
+                        {parameters: {selected: newSelectedParameters, available: newAvailableParameters}});
                     return;
                 case 'locations':
                     const newAvailableLocations = collectLocations(sensors);
                     let location= state.locations.selected;
 
-                    //comment this out since newAvailableLocations is not an array of string so cannot use newAvailableLocations.indexOf(location)
+                    //comment this out since newAvailableLocations is not an array of string so cannot use
+                    // newAvailableLocations.indexOf(location)
                     // if( location!== null && newAvailableLocations.indexOf(location) < 0) {
                         location = null;
                     //}
-                    newState = Object.assign({}, newState, {locations: {selected: location, available: newAvailableLocations}});
+                    newState = Object.assign({}, newState,
+                        {locations: {selected: location, available: newAvailableLocations}});
                     return;
                 case 'time':
                     const newAvailableDates = collectDates(sensors);
                     let selected = state.dates.selected;
-                    newState = Object.assign({}, {dates: {selected: {start: selected.start, end: selected.end}, available: {start: newAvailableDates.start, end: newAvailableDates.end}}});
+                    newState = Object.assign({}, {dates: {
+                        selected: {start: selected.start, end: selected.end},
+                        available: {start: newAvailableDates.start, end: newAvailableDates.end}}});
                     return;
 
             }
