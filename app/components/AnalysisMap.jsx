b0c24e80ff9a60eaf49f0efa19956640088ef1a5
Michelle Pitcel <mpitcel2@illinois.edu>
2018-03-23 16:32:55 -0500

    correct regions shapes outlines

--- app/components/AnalysisMap.jsx
+++ app/components/AnalysisMap.jsx
@@ -134,6 +134,7 @@ class AnalysisMap extends Component {
             area = getCustomTrendRegion(that.props.selectedRegion);
             if (area && area.geometry) {
                 feature = new ol.Feature({geometry: new ol.geom.Polygon(area.geometry.coordinates)});
+                feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
                 region_features.push(feature);
             }
         }
