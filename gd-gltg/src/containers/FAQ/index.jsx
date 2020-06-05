7ad500b8ace32349257b83adf19c296317eb6c65
kaveh <kaveh@illinois.edu>
2020-06-05 08:13:44 -0500

    Add HUC8 executive summary to HUC8 section in FAQ

--- gd-gltg/src/containers/FAQ/index.jsx
+++ gd-gltg/src/containers/FAQ/index.jsx
@@ -6,6 +6,7 @@ import {
     ExpansionPanel,
     ExpansionPanelSummary,
     ExpansionPanelDetails,
+    Paper,
     Typography,
     makeStyles
 } from '@material-ui/core';
@@ -26,6 +27,10 @@ const useStyle = makeStyles({
     },
     panel: {
         margin: '20px auto'
+    },
+    summary: {
+        margin: 10,
+        padding: 10
     }
 });
 
@@ -103,8 +108,40 @@ const FAQ = () => {
                 <ExpansionPanelDetails>
                     <Typography>
                         A: The data was compiled by Dr. Greg McIsaac as part of the Illinois Nutrient Loss Reduction
-                        Strategy Science Assessment and Update. <br />
-                        All data can be found here:{' '}
+                        Strategy Science Assessment and Update.
+                        Here is the summary of the report:
+                        <br />
+                        <Paper className={classes.summary} elevation={1}>
+                            The purpose of this report was to update estimates and quantify changes in riverine
+                            nitrate-N and total phosphorus (TP) loads and yields in Illinois as part of the Illinois
+                            Nutrient Loss Reduction Strategy (NLRS) process. Using river flow data from the USGS and
+                            concentration data from IEPA, USGS, Metropolitan Water Reclamation District of Greater
+                            Chicago (MWRD),the Fox River Study Group (FRSG) and University of Illinois, nitrate-N and
+                            total phosphorus (TP) loads for the eight major rivers draining Illinois were calculated
+                            through the 2017 water year and aggregated to estimate state wide annual losses.
+                            For the five year period from 2013-17 the statewide water flow, nitrate-N loads and TP
+                            loads were estimated to be 13%, 7% and 26% above the 1980-96 baseline period. Much of the
+                            increase in the nitrate load occurred in the Rock River while much of the increase in TP
+                            load occurred in the Illinois River. Point source discharges of total N (TN) and TP for the
+                            2017 calendar year were provided by IEPA. Statewide, point source TN discharge was about 75
+                            million lb/yr, or about 14% less than the previous estimate for 2011. TP discharge from
+                            point sources for 2017 was estimated to be 14 million lb/yr or about 22% less than the 2011
+                            estimate of 18 million lb/yr. Nitrate and TP yields were also estimated for the eight digit
+                            hydrologic units (HUC8s). In general, 2012-17 nitrate-N yields were similar to values
+                            calculated for 1997-2011. For HUCs with nitrate-N yield greater than 11 lb N/ac-yr, changes
+                            in nitrate yield were correlated with change in water yield. For three HUCs in north western
+                            Illinois (Mackinaw, Spoon, and Flint Henderson) there appeared to be some reduction in
+                            nitrate-N yield independent of changes in water yield. Changes in estimation methods used
+                            for the Lower Illinois River and Lower Sangamon River resulted in lower estimates of
+                            nitrate-N loads for these HUCs. Reductions in TP yield from the Des Plaines and Chicago HUCs
+                            of 15 and 27%, respectively, corresponded to reductions in point source discharges in those
+                            HUCs. On the other hand, increases in TP yield were calculated for the Upper Sangamon,
+                            Macoupin and several other HUCs. Suggestions for improving future nutrient loss assessments
+                            include more frequent river sampling, especially for phosphorus at high flows, and
+                            identifying relationships between monitored nutrient loads and watershed characteristics
+                            to estimate loads from unmonitored areas.
+                        </Paper>
+                        All data can be found here:&nbsp;
                         <a
                             target="_blank"
                             rel="noopener noreferrer"
