45c534167aeffe28a0187da7f33357e04f88a94d
Aaraj Habib <aarajh@illinois.edu>
2020-10-16 10:21:35 -0500

    Smartfarm Fix material styles errors

--- gd-smartfarm/src/containers/Layout/index.jsx
+++ gd-smartfarm/src/containers/Layout/index.jsx
@@ -14,8 +14,6 @@ const useStyles = makeStyles({
         zIndex: 2000
     },
     main: {
-        display: 'flex',
-        flexDirection: 'column',
         position: 'absolute',
         top: HEADERS_HEIGHT,
         width: '100%',
