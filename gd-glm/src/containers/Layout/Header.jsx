6e77e34b18d7d78be35a9a4866fb3cff635016cd
Aaraj Habib <aarajh@illinois.edu>
2020-10-23 19:03:36 -0500

    Update glm header

--- gd-glm/src/containers/Layout/Header.jsx
+++ gd-glm/src/containers/Layout/Header.jsx
@@ -1,182 +1,70 @@
-// @flow
-import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/*
+ * @flow
+ */
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
 import {
-    AppBar,
-    Avatar,
-    ClickAwayListener,
-    MenuItem,
-    Paper,
-    Popper,
-    Tab,
-    Tabs,
-    Toolbar,
-    Typography,
-    makeStyles
+    ClickAwayListener
 } from '@material-ui/core';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import LogoApp from '../../images/logo_app.png';
+import LogoApp from '../../images/logo.png';
 
 export const HEADERS_HEIGHT = 132;
 
-const useStyles = makeStyles((theme) =>{
-    return ({
-        mainHeader: {
-            'background': 'linear-gradient(to bottom, #054455, #467A9E)',
-            'color': theme.palette.primary.contrastText,
-            'textDecoration': 'none',
-            'height': HEADERS_HEIGHT,
-            'minHeight': HEADERS_HEIGHT,
-            '& a': {
-                margin: 5
-            }
-        },
-        logo: {
-            height: 90
-        },
-        headerText: {
-            color: theme.palette.primary.contrastText,
-            textDecoration: 'none'
-        },
-        tabsRoot: {
-            fontSize: 16,
-            flexGrow: 1
-        },
-        tabsIndicator: {
-            backgroundColor: '#fff'
-        },
-        tabRoot: {
-            fontSize: '1rem'
-        },
-        marginLeftAuto: {
-            marginLeft: 'auto !important'
-        },
-        dropdown: {
-            zIndex: 1100
-        },
-        dropdownIcon: {
-            display: 'flex'
-        }
-    });
-});
-
-type Props = {
-    location: {
-        pathname: string
-    }
+function DropDown({ label = '', children }: Object) {
+    const [isActive, toggleActive] = useState(false);
+    return(
+        <>
+            <a 
+                className="dropDownLink" 
+                onClick={() => toggleActive(!isActive)}> 
+                {label} &#9660;
+            </a>
+            {isActive ?
+                <ClickAwayListener onClickAway={() => toggleActive(false)}>
+                    <div className="dropdownList" id="dropdownItemsTrends">
+                        {children}
+                    </div>
+                </ClickAwayListener> :
+                null}
+        </>
+    );
 }
 
-const Header = ({ location }: Props) => {
-    const classes = useStyles();
-
-    const geostreamingMenuEl = React.useRef(null);
-    const [geostreamingMenuOpen, updateGeostreamingMenuOpen] = React.useState(false);
+const Header = () => {
 
     return (
-        <AppBar position="fixed">
-            <Toolbar className={classes.mainHeader}>
-                <Avatar
-                    className={classes.logo}
-                    component={Link}
-                    to="/"
-                    src={LogoApp}
-                />
-                <Typography
-                    component={Link}
-                    to="/"
-                    className={classes.headerText}
-                    variant="h6"
-                    noWrap
-                >
-                    Great Lakes Monitoring
-                </Typography>
-                <Tabs
-                    classes={{
-                        root: classes.tabsRoot,
-                        indicator: classes.tabsIndicator
-                    }}
-                    centered
-                    value={location.pathname.split('/')[1]}
-                >
-                    <Tab
-                        className={`${classes.marginLeftAuto} ${classes.tabRoot}`}
-                        label="Home"
-                        component={Link}
-                        to="/"
-                        value=""
-                    />
-                    <Tab
-                        className={classes.tabRoot}
-                        label="Data Stories"
-                        component={Link}
-                        to="/data-stories"
-                        value="data-stories"
-                    />
-                    <Tab
-                        className={classes.tabRoot}
-                        label="Partners"
-                        component={Link}
-                        to="/partners"
-                        value="partners"
-                    />
-                    <Tab
-                        className={classes.tabRoot}
-                        label="FAQ"
-                        component={Link}
-                        to="/faq"
-                        value="faq"
-                    />
-                    <Tab
-                        ref={geostreamingMenuEl}
-                        component="a"
-                        className={`${classes.marginLeftAuto} ${classes.tabRoot}`}
-                        label={
-                            <span className={classes.dropdownIcon}>
-                                Geostreaming App <ArrowDropDownIcon />
-                            </span>
-                        }
-                        value="geostreaming"
-                        onClick={() => updateGeostreamingMenuOpen(true)}
-                    />
-                    <Popper
-                        className={classes.dropdown}
-                        anchorEl={geostreamingMenuEl.current}
-                        open={geostreamingMenuOpen}
-                    >
-                        <ClickAwayListener
-                            onClickAway={() => updateGeostreamingMenuOpen(false)}
-                        >
-                            <Paper>
-                                <MenuItem
-                                    component={Link}
-                                    to="/geostreaming"
-                                >
-                                    About
-                                </MenuItem>
-                                <MenuItem
-                                    component={Link}
-                                    to="/geostreaming/explore/all"
-                                >
-                                    Explore
-                                </MenuItem>
-                                <MenuItem
-                                    component={Link}
-                                    to="/geostreaming/search"
-                                >
-                                    Download
-                                </MenuItem>
-                                <MenuItem
-                                    component={Link}
-                                    to="/geostreaming/analysis"
-                                >
-                                    Analysis
-                                </MenuItem>
-                            </Paper>
-                        </ClickAwayListener>
-                    </Popper>
-                </Tabs>
-            </Toolbar>
-        </AppBar>
+        <header className="header_banner">
+            <div>
+                <img className="header_image" src={LogoApp} alt="GLM Logo" />
+            </div>
+            <div className="header_title">
+                <p id="header-title-text" className="header_title_text">
+                Great Lakes Monitoring
+                </p>
+            </div>
+            <div className="header_subtitle">
+                <p id="header-subtitle-text" className="header_subtitle_text">
+                ILLINOIS-INDIANA SEA GRANT </p>
+            </div>
+            <div className="nav">
+                <ul id="sidebar">
+                    <li className="welcome"><a href="/">WELCOME</a></li>
+                    <li className="explore"><a href="/explore/all">EXPLORE</a></li>
+                    <li className="download"><a href="/search">DOWNLOAD</a></li>
+                    <li className="dropdown">
+                        <DropDown label="TRENDS">
+                            <a href="/trendsstations">TRENDS STATIONS</a>
+                            <a href="/trendsregions">TRENDS REGIONS</a>
+                        </DropDown>
+                    </li>
+
+                    <li className="analysis"><a href="/about">ABOUT</a></li>
+                </ul>
+            </div>
+        </header>
     );
 };
 
