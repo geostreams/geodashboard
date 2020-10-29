debaa272c09a5c17523bdcc58bf343660d124eb1
Aaraj Habib <aarajh@illinois.edu>
2020-10-29 13:20:27 -0500

    Allow render method to accept custom theme

--- gd-core/src/App.jsx
+++ gd-core/src/App.jsx
@@ -3,14 +3,15 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 
-import theme from './theme';
+import defaultTheme from './theme';
 import { entries } from './utils/array';
 
 type Props = {
     routes: { [key: string]: Function };
+    theme: Object
 }
 
-const App = ({ routes }: Props) => (
+const App = ({ routes, theme = defaultTheme }: Props) => (
     <ThemeProvider theme={theme}>
         {entries(routes).map(([path, props]) => (
             <Route key={path} path={`${process.env.CONTEXT || ''}${path}`} {...props} />
