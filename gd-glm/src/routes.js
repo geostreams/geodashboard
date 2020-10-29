c619f815d09d9ca942bb2a5e426c71bcb5e5fbcd
Aaraj Habib <aarajh@illinois.edu>
2020-10-29 14:09:32 -0500

    Updated routes to point to gd-core__old

--- gd-glm/src/routes.js
+++ gd-glm/src/routes.js
@@ -3,24 +3,24 @@ import hocs from 'gd-core/src/utils/hocs';
 import SensorDetail from 'gd-geostreaming/src/containers/Sensor/Detail';
 
 // $FlowFixMe
-import __old_Explore from 'gd-glm__old/app/pages/Explore';
+import __old_Explore from 'gd-core__old/app/pages/Explore';
 // $FlowFixMe
-import __old_Search from 'gd-glm__old/app/pages/Search';
+import __old_Search from 'gd-core__old/app/pages/Search';
 // $FlowFixMe
-import __old_TrendsStation from 'gd-glm__old/app/pages/TrendsSensor';
+import __old_TrendsStation from 'gd-core__old/app/pages/TrendsSensor';
 // $FlowFixMe
-import __old_TrendsRegion from 'gd-glm__old/app/pages/TrendsRegion';
+import __old_TrendsRegion from 'gd-core__old/app/pages/TrendsRegion';
 // $FlowFixMe
-import __old_TrendsDetail from 'gd-glm__old/app/pages/TrendsDetail';
+import __old_TrendsDetail from 'gd-core__old/app/pages/TrendsDetail';
 // $FlowFixMe
-import __old_Analysis from 'gd-glm__old/app/pages/Analysis';
+import __old_Analysis from 'gd-core__old/app/pages/Analysis';
 import GLMLayout from './containers/Layout';
-import __old_About from './containers/About';
-import __old_Home from './containers/Home';
+import About from './containers/About';
+import Home from './containers/Home';
 
 const routes = {
-    '/': { exact: true, component: hocs.withLayout(GLMLayout, __old_Home, { hasFooter: true }) },
-    '/about': { exact: true, component: hocs.withLayout(GLMLayout, __old_About, { hasFooter: true }) },
+    '/': { exact: true, component: hocs.withLayout(GLMLayout, Home, { hasFooter: true }) },
+    '/about': { exact: true, component: hocs.withLayout(GLMLayout, About, { hasFooter: true }) },
     '/explore/:stations': { component: hocs.withLayout(GLMLayout,__old_Explore), exact: true },
     '/:parent(explore|trendsstations)/detail/location/:name/:category': { component: hocs.withLayout(GLMLayout,SensorDetail) },
     '/search': { component: hocs.withLayout(GLMLayout,__old_Search) },
