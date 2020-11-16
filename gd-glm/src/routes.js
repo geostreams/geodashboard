200015aba62f25ba7d237e9d685e723d3c63c7cd
Aaraj Habib <aarajh@illinois.edu>
2020-11-16 09:22:57 -0600

    Use gd-glm__old for geostreaming pages for now

--- gd-glm/src/routes.js
+++ gd-glm/src/routes.js
@@ -3,17 +3,17 @@ import hocs from 'gd-core/src/utils/hocs';
 import SensorDetail from 'gd-geostreaming/src/containers/Sensor/Detail';
 
 // $FlowFixMe
-import __old_Explore from 'gd-core__old/app/pages/Explore';
+import __old_Explore from 'gd-glm__old/app/pages/Explore';
 // $FlowFixMe
-import __old_Search from 'gd-core__old/app/pages/Search';
+import __old_Search from 'gd-glm__old/app/pages/Search';
 // $FlowFixMe
-import __old_TrendsStation from 'gd-core__old/app/pages/TrendsSensor';
+import __old_TrendsStation from 'gd-glm__old/app/pages/TrendsSensor';
 // $FlowFixMe
-import __old_TrendsRegion from 'gd-core__old/app/pages/TrendsRegion';
+import __old_TrendsRegion from 'gd-glm__old/app/pages/TrendsRegion';
 // $FlowFixMe
-import __old_TrendsDetail from 'gd-core__old/app/pages/TrendsDetail';
+import __old_TrendsDetail from 'gd-glm__old/app/pages/TrendsDetail';
 // $FlowFixMe
-import __old_Analysis from 'gd-core__old/app/pages/Analysis';
+import __old_Analysis from 'gd-glm__old/app/pages/Analysis';
 import GLMLayout from './containers/Layout';
 import About from './containers/About';
 import Home from './containers/Home';
