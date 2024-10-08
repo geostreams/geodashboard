beabb6f05606ed7a09e292d504b5cde30b435952
Vismayak Mohanarajan <mohanar2@illinois.edu>
2024-10-08 15:04:00 -0500

    Update render.jsx

--- packages/core/src/render.jsx
+++ packages/core/src/render.jsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
